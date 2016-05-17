b974961542359943355f96d93b8fb0eb17095e3e
wuyanjun <wuyanjun@hisilicon.com>
2016-05-17 19:51:23 +0800

    modify the variable name in mysql-server installation

--- distro/opensuse/scripts/opensuse_expect_mysql.sh
+++ distro/opensuse/scripts/opensuse_expect_mysql.sh
@@ -5,8 +5,8 @@ set mysql_password [lindex $argv 0]
 
 spawn zypper -n install mysql-server
 expect "New password for the MySQL"
-send "$mysql_password\r"
+send "${mysql_password}\r"
 expect "Repeat password for the MySQL"
-send "$mysql_password\r"
+send "${mysql_password}\r"
 expect eof
 
