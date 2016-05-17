f5616955a777659128924c7ae1d9a4e34b1854fb
wuyanjun <wuyanjun@hisilicon.com>
2016-05-17 11:43:00 +0800

    add the samba for ubuntu

--- common/scripts/samba.sh
+++ common/scripts/samba.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
-server_ip=$(ifconfig `route -n | grep "^0"|awk '{print $NF}'`|grep -o "addr inet:[0-9\.]*"|cut -d':' -f 2)
-if [ "$server_ip"x = ""x ]; then
-    server_ip=$(ip addr show `ip route | grep "default" | awk '{print $NF}'`| grep -o "inet [0-9\.]*" | cut -d" " -f 2)
-fi
-. ../../common.sh
 
-sudo useradd -p '123ABS' smb
+. ../../distro/common/utils/sys_info.sh
+
+$install_commands samba smbfs smbclient 
+#sudo useradd -p '123ABS' smb
 
 if [ ! -d /opt/samba/share ]; then
     sudo mkdir -p /opt/samba/share
@@ -30,11 +28,19 @@ else
     echo "samba test succ"
     lava-test-case samba-server-restart pass
 fi
-
 echo $! > /tmp/samba-server.pid
-lava-send server-ready server_ip=${server_ip}
-lava-wait client-done
+
+#### for client visit ####
+TEST_CASE="samba-visit-share"
+
+echo "Running samba client "
+echo -n "${TEST_CASE}: "
+smbclient ${local_ip}/share 2>&1 | tee result.log
+echo -n "${TEST_CASE}: "
+lava-test-case samba-client --shell [ -z "`grep "Connection refused" result.log`" ] && true || false
+lava-test-case-attach smaba-client result.log
+#### client visit ending ####
 
 echo "samba server:"
-echo "Server IP: " ${server_ip}
+echo "Server IP: " ${local_ip}
 lava-test-case samba-server --shell kill -9 `cat /tmp/samba-server.pid`
